17c69010543119c2db09bf05fd1818d2f9cc5a27
Gary Passero <gpassero@gmail.com>
2026-04-02 15:17:38 -0400

    Add composer method param

--- spec/models/composable_query_spec.rb
+++ spec/models/composable_query_spec.rb
@@ -8,40 +8,57 @@ module PackAPI::Mapping
       let!(:foo) { BlogPost.create!(title: 'foo') }
       let!(:bar) { BlogPost.create!(title: 'bar') }
 
-      let(:query) { described_class.new(BlogPost.all) }
 
-      it 'can add where clauses' do
-        # when
-        query.add(BlogPost.where(title: 'foo'))
+      context 'with composer = :merge' do
+        let(:query) { described_class.new(BlogPost.all, composer: :merge) }
 
-        # then
-        result = query.build.to_a
-        expect(result).to have(1).item
-        expect(result.first.title).to eq('foo')
+        it 'can add where clauses' do
+          # when
+          query.add(BlogPost.where(title: 'foo'))
+
+          # then
+          result = query.build.to_a
+          expect(result).to have(1).item
+          expect(result.first.title).to eq('foo')
+        end
+
+        it 'can add join clauses and dependent where clauses' do
+          # given
+          baz = Author.create!(name: 'baz')
+          foo.update(author_id: baz.id)
+          # when
+          query.add(BlogPost.joins("JOIN authors on authors.id = blog_posts.author_id")
+                            .where("authors.name = 'baz'"))
+          # then
+          result = query.build.to_a
+          expect(result).to have(1).item
+          expect(result.first.title).to eq('foo')
+        end
+
+        it 'can add projections to result' do
+          # when
+          query.add(BlogPost.select('id baz'))
+
+          # then
+          result = query.build.to_a
+          expect(result).to have(2).items
+          result_baz = result.pluck(:baz)
+          expect(result_baz).not_to be_nil
+        end
       end
 
-      it 'can add join clauses and dependent where clauses' do
-        # given
-        baz = Author.create!(name: 'baz')
-        foo.update(author_id: baz.id)
-        # when
-        query.add(BlogPost.joins("JOIN authors on authors.id = blog_posts.author_id")
-                          .where("authors.name = 'baz'"))
-        # then
-        result = query.build.to_a
-        expect(result).to have(1).item
-        expect(result.first.title).to eq('foo')
-      end
+      context 'with composer = :and' do
+        let(:query) { described_class.new(BlogPost.all, composer: :and) }
 
-      it 'can add projections to result' do
-        # when
-        query.add(BlogPost.select('id baz'))
+        it 'can add where clauses' do
+          # when
+          query.add(BlogPost.where(title: 'foo'))
 
-        # then
-        result = query.build.to_a
-        expect(result).to have(2).items
-        result_baz = result.pluck(:baz)
-        expect(result_baz).not_to be_nil
+          # then
+          result = query.build.to_a
+          expect(result).to have(1).item
+          expect(result.first.title).to eq('foo')
+        end
       end
     end
   end
