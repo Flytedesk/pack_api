b315f968c7e23cf26324f5033bb6b04a8105bc89
Gary Passero <gpassero@gmail.com>
2026-04-03 10:33:41 -0400

    Update DynamicEnumFilter interface to support lookup of selected options (#13)
    For dynamic enum filters, the set of options is derived from user input. This commit expands the supported
    interaction to recognize 2 new scenarios:
    * persisted values - when rendering a dynamic enum filter with persisted values, those values need to be correlated with options (label + value).
    * copy/paste - when pasting values, the options need to be fetched with labels.

--- lib/pack_api/querying/dynamic_enum_filter.rb
+++ lib/pack_api/querying/dynamic_enum_filter.rb
@@ -6,10 +6,17 @@ module PackAPI::Querying
     class_methods do
       def type = :dynamic_enum
 
-      def search_for_options(relation:, search:) = raise NotImplementedError
+      ###
+      # Retrieve options for this filter based on user input.
+      # @param relation [ActiveRecord::Relation] The base relation to query against for options.
+      # @param search [String, nil] An optional search term to filter options by (using wildcard matching)
+      # @param value [String, Array<String>, nil] An optional value or array of values to filter options by (exact match)
+      def dynamic_options(relation:, search: nil, value: nil) = raise NotImplementedError
 
       private
 
+      ###
+      # Static filter options is an empty array.
       def filter_options(**) = PackAPI::FrozenEmpty::ARRAY
     end
   end
